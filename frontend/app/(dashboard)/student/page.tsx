a6850f0d64b5281b9591b2ff6fadcdf53333eb76
Kramik <markexconde31@gmail.com>
2025-09-27 20:12:18 +0800

    refactor: update layout structure for student page

--- frontend/app/(dashboard)/student/page.tsx
+++ frontend/app/(dashboard)/student/page.tsx
@@ -3,19 +3,19 @@ import { AppAreaChart } from "@/components/AppAreaChart"
 
 const page = () => {
   return (
-    <div className="p-0 flex gap-4 flex-col md:flex-row">
-      {/*LEFT*/}
-      <div className="w-full lg:w-2/3 flex flex-col gap-4">
-          <UserCards />   
-        <div className="">
-          <AppAreaChart />
-        </div>
-      </div>
-      {/*RIGHT*/}
-      <div className="w-full lg:w-1/3">
-      RIGHT
-      </div>
-    </div>
+<div className="p-0 flex gap-4 flex-col lg:flex-row">
+  {/* LEFT */}
+  <div className="flex-1 min-w-0 flex flex-col gap-4">
+    <UserCards />
+    <AppAreaChart />
+  </div>
+
+  {/* RIGHT */}
+  <div className="w-full lg:w-1/3 min-w-0">
+    RIGHT
+  </div>
+</div>
+
   )
 }
 

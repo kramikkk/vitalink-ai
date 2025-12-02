bb5b4fdcb90b93b12dcb69ebd76fd4d60a0fe998
Kramik <markexconde31@gmail.com>
2025-12-02 15:33:11 +0800

    feat: update LoginPage and SignUpPage to use DarkVeil component for enhanced background effects; add DarkVeil component for customizable visual effects

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+
+import DarkVeil from "@/components/DarkVeil"
 import { SignUpForm } from "@/components/SignUpForm"
 
 export default function SignUpPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="relative flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="absolute inset-0 z-[-1] h-full w-full">
+        <DarkVeil 
+          hueShift={57}
+        />
+      </div>
       <div className="w-full max-w-sm md:max-w-6xl">
         <SignUpForm />
       </div>
